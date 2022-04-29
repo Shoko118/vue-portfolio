1284e222507c3f8f8dbab89599b7e175335a1f37
Shoko118 <secast118@gmail.com>
2022-04-30 00:48:32 +0800

    changing content

--- src/pages/AboutMe.vue
+++ src/pages/AboutMe.vue
@@ -23,8 +23,7 @@
               <span class="underline underline-offset-4 decoration-green-700">Frontend curriculum</span> and love to
               work with people on idea projects ideas, consistently
               <span class="underline underline-offset-4 decoration-teal-600">learning techs</span> and road to becoming
-              a <span class="underline underline-offset-4 decoration-red-500">better developer.</span> Fan of John
-              Mayer, K-show entertainment, guitar lover 🎸, and the one and only me.
+              a <span class="underline underline-offset-4 decoration-red-500">better developer.</span>
             </p>
             <div class="mt-10">
               If you happen to be interested in techs, music, or anything would love to grab a burger with you. Add me
